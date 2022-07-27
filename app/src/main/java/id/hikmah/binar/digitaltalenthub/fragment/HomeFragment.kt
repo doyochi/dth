8b269d19075154a14111efe1cf411f4beb8b257e
doyochi <nuzululhikmah117@gmail.com>
2022-07-27 11:27:43 +0700

    init recycleview, add job

--- app/src/main/java/id/hikmah/binar/digitaltalenthub/fragment/HomeFragment.kt
+++ app/src/main/java/id/hikmah/binar/digitaltalenthub/fragment/HomeFragment.kt
@@ -12,10 +12,14 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.view.isGone
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.textfield.TextInputLayout
 import id.hikmah.binar.digitaltalenthub.AuthActivity
 import id.hikmah.binar.digitaltalenthub.R
+import id.hikmah.binar.digitaltalenthub.adapter.JobActionListener
+import id.hikmah.binar.digitaltalenthub.adapter.JobAdapter
+import id.hikmah.binar.digitaltalenthub.database.JobEntity
 import id.hikmah.binar.digitaltalenthub.database.UserDatabase
 import id.hikmah.binar.digitaltalenthub.databinding.FragmentHomeBinding
 import kotlinx.coroutines.CoroutineScope
@@ -28,6 +32,7 @@ class HomeFragment : Fragment() {
     private val binding get() = _binding!!
 
     private lateinit var sharedPref: SharedPreferences
+    private lateinit var jobAdapter: JobAdapter
     private var userDb: UserDatabase? = null
 
     override fun onCreateView(
@@ -48,7 +53,122 @@ class HomeFragment : Fragment() {
         sharedPref = requireContext().getSharedPreferences("SHARED_PREF", Context.MODE_PRIVATE)
         userDb = UserDatabase.getInstance(requireContext())
 
+        initRecyclerView()
+        getJobData()
+        welcomeUsername()
         actionLogout()
+        addJob()
+    }
+
+    private fun welcomeUsername() {
+        val getUsername = sharedPref.getString("USERNAME", "Default Value")
+        Toast.makeText(requireContext(), "Welcome, $getUsername", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun initRecyclerView() {
+        binding.apply {
+            jobAdapter = JobAdapter(action)
+            rvJobData.adapter = jobAdapter
+            rvJobData.layoutManager = LinearLayoutManager(requireContext())
+        }
+    }
+    private fun addJob() {
+        binding.btnTambah.setOnClickListener {
+            showCustomDialog(null)
+        }
+    }
+
+    private fun showCustomDialog(job: JobEntity?) {
+        val customLayout = LayoutInflater.from(requireContext()).inflate(R.layout.layout_tambah_job, null, false)
+        val dialogBuilder = AlertDialog.Builder(requireContext())
+
+        val tvTitleDialog = customLayout.findViewById<TextView>(R.id.title_dialog)
+        val etTitle = customLayout.findViewById<TextInputLayout>(R.id.edit_title)
+        val etNote = customLayout.findViewById<TextInputLayout>(R.id.edit_note)
+        val etSalary = customLayout.findViewById<TextInputLayout>(R.id.edit_salary)
+        val btnAddUpdate = customLayout.findViewById<Button>(R.id.btn_add_update)
+
+        if (job != null) {
+            etTitle.editText?.setText(job.jobTitle)
+            etNote.editText?.setText(job.desc)
+            etSalary.editText?.setText(job.salary)
+            tvTitleDialog.text = "Edit"
+            btnAddUpdate.text = "Perbarui"
+        }
+
+        dialogBuilder.setView(customLayout)
+        val dialog = dialogBuilder.create()
+
+        btnAddUpdate.setOnClickListener {
+            val title = etTitle.editText?.text.toString()
+            val desc = etNote.editText?.text.toString()
+            val salary = etSalary.editText?.text.toString().toInt()
+
+            if (job != null) {
+                val newJob = JobEntity(job.id, job.idUser, title, desc, salary)
+                updateToDb(newJob)
+            } else {
+                insertToDb(title, desc, salary )
+            }
+            dialog.dismiss()
+        }
+        dialog.show()
+    }
+
+    private fun insertToDb(title: String, desc: String, salary: Int) {
+        val getUserId = sharedPref.getInt("USERID", 0)
+        val job = JobEntity(null, getUserId, title, desc,salary)
+        CoroutineScope(Dispatchers.IO).launch {
+            val result = userDb?.userDao()?.addJob(job)
+            if (result != 0L) {
+                getJobData()
+                CoroutineScope(Dispatchers.Main).launch {
+                    Toast.makeText(requireContext(),
+                        "Berhasil menambahkan job", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                CoroutineScope(Dispatchers.Main).launch {
+                    Toast.makeText(requireContext(),
+                        "Gagal menambahkan job", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun updateToDb(job: JobEntity) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val result = userDb?.userDao()?.updateJob(job)
+            if (result != 0) {
+                getJobData()
+                CoroutineScope(Dispatchers.Main).launch {
+                    Toast.makeText(requireContext(),
+                        "Berhasil diperbarui", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                CoroutineScope(Dispatchers.Main).launch {
+                    Toast.makeText(requireContext(),
+                        "Gagal diperbarui", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun getJobData() {
+        val getUserId = sharedPref.getInt("USERID", 0)
+        CoroutineScope(Dispatchers.IO).launch {
+            val result = userDb?.userDao()?.getAllJob(getUserId)
+            if (!result.isNullOrEmpty()) {
+                CoroutineScope(Dispatchers.Main).launch {
+                    jobAdapter.updateDataRecycler(result)
+                    binding.txtNoteEmpty.isGone = true
+                }
+            } else {
+                CoroutineScope(Dispatchers.Main).launch {
+                    jobAdapter.updateDataRecycler(result!!)
+                    binding.txtNoteEmpty.isGone = false
+                }
+            }
+        }
     }
 
     private fun actionLogout() {
@@ -62,6 +182,17 @@ class HomeFragment : Fragment() {
                 requireActivity().finish()
             }
 
+        }
+    }
+
+    //recycle view action listener
+    private val action = object : JobActionListener {
+        override fun onDelete(job: JobEntity) {
+//            showDeleteDialog(job)
+        }
+
+        override fun onEdit(job: JobEntity) {
+            showCustomDialog(job)
         }
     }
 
